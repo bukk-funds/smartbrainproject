b179a47d1bc675904bc9ca9255fc22da7fded3d7
chibunkem <chibunkemukonu2020@gmail.com>
2022-11-23 18:33:02 +0100

    another commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser'); // latest version of exressJS now comes with Body-Parser!
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex')
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+      host: '127.0.0.1',
+      user: 'postgres',
+      password: 'admintestpost',
+      database: 'smartbrain'
+  }
+});
+
+const app = express();
+
+app.use(cors())
+app.use(express.json()); 
+
+app.post('/signin', (req, res) => { signin.handleLogin(req, res, db, bcrypt) })
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res) })
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+
+app.listen(process.env.PORT || 3000, ()=> {
+  console.log(`app is running on port ${process.env.PORT}`);
+})
